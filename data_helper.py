3fcded41bce19b18d9a32a813c69ac0090b987a7
Jonathan Guiang <jkguiang@gmail.com>
2020-07-01 09:18:35 -0700

    cleaned up

--- data_helper.py
+++ data_helper.py
@@ -9,7 +9,7 @@ import random
 import utils
 
 
-class Data_Helper():
+class DataHelper():
     def __init__(self, **kwargs):
         self.kwargs = kwargs
 
@@ -25,14 +25,22 @@ class Data_Helper():
         self.tag        = kwargs.get('tag')
         
         self.downsample = kwargs.get('downsample')
-        self.min_size   = kwargs.get('min_size', 512) # automatically downsample all images to this size to begin with (some inputs may be bigger, some will be this size)
+        # Automatically downsample all images to this size to begin with (some 
+        # inputs may be bigger, some will be this size)
+        self.min_size   = kwargs.get('min_size', 512) 
 
         self.preprocess_method = kwargs.get('preprocess_method', 'z_score')
 
-        self.output_dir = self.input_dir + "/features/" + self.tag + "_" + self.preprocess_method + "_downsample%d/" % self.downsample
+        self.output_dir = (self.input_dir 
+                           + "/features/" 
+                           + self.tag 
+                           + "_" 
+                           + self.preprocess_method 
+                           + "_downsample%d/" % self.downsample)
 
         if os.path.exists(self.output_dir):
-            print("[DATA_HELPER] Cleaning old contents in directory: %s" % self.output_dir)
+            print("[DATA_HELPER] Cleaning old contents in directory: %s" 
+                  % self.output_dir)
             os.system("rm -rf %s" % self.output_dir)
 
         os.system("mkdir %s" % self.output_dir)
@@ -47,24 +55,27 @@ class Data_Helper():
                 self.metadata = json.load(f_in)
         else:
             self.metadata = {
-                "input_dir_wuhan" : self.input_dir_wuhan,
-                "input_dir_russia" : self.input_dir_russia,
-                "downsample" : self.downsample,
-                "preprocess_scheme" : { "method" : self.preprocess_method },
-                "output" : [],
-                "diagnostic_plots" : []
+                "input_dir_wuhan": self.input_dir_wuhan,
+                "input_dir_russia": self.input_dir_russia,
+                "downsample": self.downsample,
+                "preprocess_scheme": {"method" : self.preprocess_method},
+                "output": [],
+                "diagnostic_plots": []
             }
 
     def prep(self):
         self.find_patients()
 
         ctr = 0
+        n_patients = len(self.patients.keys())
         for patient, data in self.patients.items():
             ctr += 1
-            print("[DATA_HELPER] On patient %d out of %d (%.1f%%)" % (ctr, len(self.patients.keys()), (100. * float(ctr)) / float(len(self.patients.keys()))))
+            print("[DATA_HELPER] On patient %d out of %d (%.1f%%)" 
+                  % (ctr, n_patients, (100.*float(ctr))/float(n_patients)))
             
             if os.path.exists(self.output_file(patient)):
-                print("[DATA_HELPER] File %s already exists, we will continue and not remake it." % self.output_file(patient)) 
+                print("[DATA_HELPER] File %s already exists, continuing." 
+                      % self.output_file(patient)) 
                 continue
             self.load_data(patient)
             self.assess(patient, 10, "raw")
@@ -72,7 +83,8 @@ class Data_Helper():
             self.preprocess(patient)
             self.assess(patient, 10, "processed")
             self.write(patient)
-            self.patients[patient] = {} # release memory when we're done with the patient
+            # Release memory when we're done with the patient
+            self.patients[patient] = {} 
 
         self.f_out.close()
         self.write_metadata()
@@ -84,20 +96,22 @@ class Data_Helper():
             for dir in glob.glob(self.input_dir_wuhan + "/patient*/"):
                 patient = dir.split("/")[-2]
                 self.patients[patient] = {
-                        "inputs" : glob.glob(dir + "*/"),
-                        "X" : [],
-                        "y" : []
+                    "inputs": glob.glob(dir+"*/"),
+                    "X": [],
+                    "y": []
                 }
 
         if self.input_dir_russia:
             for nii in glob.glob(self.input_dir_russia + "/study_*.nii.gz"):
-                id = int(nii.split("/")[-1].replace("study_", "").replace(".nii.gz", ""))
+                file_name = nii.split("/")[-1].replace(".nii.gz", "")
+                id = int(file_name.replace("study_", ""))
                 patient = "russia_patient_%d" % id
                 self.patients[patient] = {
-                        "features" : nii, 
-                        "label"    : self.input_dir_russia + "/masks/study_0%d_mask.nii.gz" % id,
-                        "X" : [],
-                        "y" : []
+                    "features": nii, 
+                    "label": (self.input_dir_russia 
+                              + "/masks/study_0%d_mask.nii.gz" % id),
+                    "X": [],
+                    "y": []
                 }
 
     def load_data(self, patient):
@@ -109,57 +123,76 @@ class Data_Helper():
 
         else: # wuhan format
             for subdir in self.patients[patient]["inputs"]:
-                print("[DATA_HELPER] Extracting data from directory %s" % subdir)
+                print("[DATA_HELPER] Extracting data from directory %s" 
+                      % subdir)
                 X = utils.load_dcms(glob.glob(subdir + "/*.dcm"))
                 y = utils.load_nii(subdir + "/ct_mask_covid_edited.nii")
                 if X is None or y is None:
-                    print("[DATA_HELPER] Did not load features or labels from directory %s, skipping." % subdir)
+                    print("[DATA_HELPER] Did not load features or labels from \
+                           directory %s, skipping." % subdir)
                     continue
                 if X.shape != y.shape:
-                    print("[DATA_HELPER] Input features have shape %s but label has shape %s -- please check!" % (str(X.shape), str(y.shape)))
+                    print("[DATA_HELPER] Input features have shape %s but label \
+                           has shape %s -- please check!" 
+                          % (str(X.shape), str(y.shape)))
                     #sys.exit(1)
                     continue
                 if X.shape[1] < self.min_size:
-                    print("[DATA_HELPER] Images are assumed to be at least %d x %d pixels, but this image is %d x %d pixels -- please check!" % (self.min_size, self.min_size, X.shape[1], X.shape[1]))
-                    #sys.exit(1)
+                    print("[DATA_HELPER] Images are assumed to be at least \
+                           %d x %d pixels, but this image is %d x %d pixels \
+                           -- please check!" 
+                           % (self.min_size, self.min_size, X.shape[1], X.shape[1]))
                     continue
                 elif X.shape[1] > self.min_size:
-                    print("[DATA_HELPER] Images are assumed to be as small as %d x %d pixels, and this image is %d x %d pixels, so we resize it down to be compatible with the rest." % (self.min_size, self.min_size, X.shape[1], X.shape[1]))
+                    print("[DATA_HELPER] Images are assumed to be as small as \
+                           %d x %d pixels, and this image is %d x %d pixels, \
+                           so we resize it down to be compatible with the rest." 
+                          % (self.min_size, self.min_size, X.shape[1], X.shape[1]))
                     X = utils.downsample_images(X, self.min_size)
                     y = utils.downsample_images(y, self.min_size, round = True)
 
             self.add_data(patient, X, y)
 
     def add_data(self, patient, X, y):
-        if len(self.patients[patient]["X"]) == 0:
-            self.patients[patient]["X"] = X
-            self.patients[patient]["y"] = y
+        p = patient
+        if len(self.patients[p]["X"]) == 0:
+            self.patients[p]["X"] = X
+            self.patients[p]["y"] = y
         else:
-            self.patients[patient]["X"] = numpy.concatenate([self.patients[patient]["X"], X])
-            self.patients[patient]["y"] = numpy.concatenate([self.patients[patient]["y"], y])
+            self.patients[p]["X"] = numpy.concatenate([self.patients[p]["X"], X])
+            self.patients[p]["y"] = numpy.concatenate([self.patients[p]["y"], y])
         
     def downsample_data(self, patient):
         #for patient, data in self.patients.items():
-        self.patients[patient]["X"] = utils.downsample_images(self.patients[patient]["X"], self.downsample)
-        self.patients[patient]["y"] = utils.downsample_images(self.patients[patient]["y"], self.downsample, round = True)
+        self.patients[patient]["X"] = utils.downsample_images(
+            self.patients[patient]["X"], 
+            self.downsample
+        )
+        self.patients[patient]["y"] = utils.downsample_images(
+            self.patients[patient]["y"], 
+            self.downsample, 
+            round = True
+        )
  
-    def assess(self, patient, N, tag): # make plots of N randomly chosen images
+    def assess(self, patient, N, tag):
+        """Make plots of N randomly chosen images"""
+        output_name = "/images/image_and_truth_%s_%s_%d.pdf" % (tag, patient, i)
         if N > 0:
             for i in range(N):
                 image, truth = self.select_random_slice(patient)    
-                output_img = self.output_dir + "/images/image_and_truth_%s_%s_%d.pdf" % (tag, patient, i)
+                output_img = self.output_dir + output_name
                 utils.plot_image_and_truth(image, truth, output_img)
                 self.metadata["diagnostic_plots"].append(output_img)
         else:
             for patient, data in self.patients.items():
                 for i in range(len(data["X"])):
                     image, truth = data["X"][i], data["y"][i]
-                    output_img = self.output_dir + "/images/image_and_truth_%s_%s_%d.pdf" % (tag, patient, i)
+                    output_img = self.output_dir + output_name
                     utils.plot_image_and_truth(image, truth, output_img)
                     self.metadata["diagnostic_plots"].append(output_img)
 
 
-    def select_random_slice(self, patient = None):
+    def select_random_slice(self, patient=None):
         if patient is None:
             patient = random.choice(list(self.patients.keys()))
         idx = random.randint(0, len(self.patients[patient]["X"]) - 1)
@@ -170,14 +203,16 @@ class Data_Helper():
             print("[DATA_HELPER] Not applying any preprocessing scheme")
             return
         elif self.preprocess_method == "z_score":
-            print("[DATA_HELPER] Applying z score transformation to hounsfeld units")
+            print("[DATA_HELPER] Applying z score transform to hounsfeld units")
             self.z_score(patient)
         else:
-            print("[DATA_HELPER] Preprocessing method %s is not supported" % self.preprocess_method)
+            print("[DATA_HELPER] Preprocessing method %s is not supported" 
+                  % self.preprocess_method)
             sys.exit(1)
 
     def z_score(self, patient):
-        if not ("mean" in self.metadata["preprocess_scheme"].keys() or "std" in self.metadata["preprocess_scheme"].keys()):
+        if not ("mean" in self.metadata["preprocess_scheme"].keys() 
+                or "std" in self.metadata["preprocess_scheme"].keys()):
             mean, std = self.calculate_mean_and_std()
             print("[DATA_HELPER] Mean: %.3f, Std dev: %.3f" % (mean, std))
             self.metadata["preprocess_scheme"]["mean"] = mean
@@ -192,12 +227,18 @@ class Data_Helper():
             if len(pixel_values) == 0:
                 pixel_values = utils.nonzero_entries(data["X"])
             else:
-                pixel_values = numpy.concatenate(pixel_values, utils.nonzero_entries(data["X"]))
+                pixel_values = numpy.concatenate(
+                    pixel_values, 
+                    utils.nonzero_entries(data["X"])
+                )
 
             if len(pixel_values) >= 10**6:
                 break
 
-        return numpy.mean(pixel_values.astype(numpy.float64)), numpy.std(pixel_values.astype(numpy.float64))
+        mean = numpy.mean(pixel_values.astype(numpy.float64))
+        std = numpy.std(pixel_values.astype(numpy.float64))
+
+        return mean, std
 
     def output_file(self, patient):
         return self.output.replace("features.hdf5", "features_%s.hdf5" % patient)
@@ -213,7 +254,11 @@ class Data_Helper():
             self.f_out.create_dataset(dset_y, data = y)
 
             n_pneumonia = str(numpy.sum(y))
-            self.metadata[patient].append( { "X" : dset_X, "y" : dset_y, "n_pneumonia" : n_pneumonia } ) 
+            self.metadata[patient].append({
+                "X": dset_X, 
+                "y": dset_y, 
+                "n_pneumonia": n_pneumonia
+            }) 
 
             idx += 1
 
