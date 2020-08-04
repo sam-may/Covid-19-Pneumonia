f8bcebd1b70248f69b0a9c844d3429efde1f59bc
Samuel May <sammay@ucla.edu>
2020-08-04 18:31:30 -0500

    Add some printouts to help debug stashcp issues

--- batch/executable.sh
+++ batch/executable.sh
@@ -17,10 +17,18 @@ echo
 # so we get good exit codes 
 set -e
 
+# pip
+pip3 install nibabel
+pip3 install pydicom
+
 # stashcache
+echo "About to run stashcp"
+ls -atlrh
 #module load stashcache
 #stashcp /osgconnect/public/smay/covid_ct_data/features/14Jul2020_z_score_downsample256/features.hdf5 .
 #stashcp /osgconnect/public/smay/covid_ct_data/features/14Jul2020_z_score_downsample256/features.json .
+echo "Finished running stashcp"
+ls -althr
 
 python3 test.py 2>&1
 
