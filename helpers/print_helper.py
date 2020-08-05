e8af69e52118cb12e66918aeb323068df2ffd9ee
Jonathan Guiang <jkguiang@gmail.com>
2020-08-05 10:39:12 -0700

    initial commit

---
+++ helpers/print_helper.py
@@ -0,0 +1,22 @@
+from __future__ import print_function
+import builtins as __builtin__
+import inspect
+
+def print(content):
+    """Print content with caller information in brackets"""
+    stack = inspect.stack()
+    caller_instance = stack[1][0]
+    # Get caller attributes
+    class_name = None
+    if "self" in caller_instance.f_locals.keys():
+        class_name = str(caller_instance.f_locals["self"].__class__.__name__)
+    func_name = str(caller_instance.f_code.co_name)
+    file_name = str(caller_instance.f_code.co_filename)
+    # Construct print name
+    caller_name = file_name
+    if class_name:
+        caller_name = class_name+"."+func_name
+    elif func_name != "<module>":
+        caller_name = func_name
+    __builtin__.print("["+caller_name+"] "+content)
+    return
