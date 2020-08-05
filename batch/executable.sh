c6660f4f48aab27f9b4f323113a906729f45dc5b
Samuel May <sammay@ucla.edu>
2020-08-04 19:40:23 -0500

    Jobs using new image and properly loading python modules

--- batch/executable.sh
+++ batch/executable.sh
@@ -17,18 +17,21 @@ echo
 # so we get good exit codes 
 set -e
 
-# pip
-pip3 install nibabel
-pip3 install pydicom
-
 # stashcache
 echo "About to run stashcp"
 ls -atlrh
+
+# need to debug why these don't work
 #module load stashcache
 #stashcp /osgconnect/public/smay/covid_ct_data/features/14Jul2020_z_score_downsample256/features.hdf5 .
 #stashcp /osgconnect/public/smay/covid_ct_data/features/14Jul2020_z_score_downsample256/features.json .
 echo "Finished running stashcp"
 ls -althr
 
-python3 test.py 2>&1
+# actual job
+echo "[executable.sh] running: tar -xvf package.tar.gz"
+tar -xvf package.tar.gz
 
+cd zephyr
+
+python3 train_unet.py
---
+++ batch/submit_jobs.py
@@ -0,0 +1,11 @@
+import os, sys
+
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("--make_tarball", help = "make tarball (overwriting if it already exists)", action="store_true")
+args = parser.parse_args()
+
+if args.make_tarball:
+    os.system("XZ_OPT='-3e -T24' tar -Jc --exclude='*.pdf' --exclude='*.hdf5' --exclude='.git' -f package.tar.gz ../../zephyr")
+
+os.system("condor_submit job.sub")
