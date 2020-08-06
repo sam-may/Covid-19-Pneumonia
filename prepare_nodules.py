7b3b2c2185f72a5dc882faf55b8c5cb371009d0c
Jonathan Guiang <jkguiang@gmail.com>
2020-08-06 22:36:31 +0000

    added CLI

--- prepare_nodules.py
+++ prepare_nodules.py
@@ -1,15 +1,21 @@
+import os
 import re
 import h5py
+import argparse
 import numpy as np
 import matplotlib.pyplot as plt
 from helpers.print_helper import print
 
 class NodulesPrepper():
-    def __init__(self):
-        self.input_hdf5 = "/mnt/data/LungNodules/besser_annotations.hdf5"
-        self.bounding_volume_shape = (64, 64, 32)
-        self.patient_regex = "^[A-Z][a-z]+_ser_\d+$"
-        self.output_hdf5 = h5py.File("features.hdf5", "w")
+    def __init__(self, input_hdf5, output_hdf5="", 
+                 bounding_volume_shape=(64,64,16), patient_regex=""):
+        self.input_hdf5 = input_hdf5
+        self.input_dir = os.path.dirname(input_hdf5)+"/"
+        self.bounding_volume_shape = bounding_volume_shape
+        self.patient_regex = patient_regex
+        if not output_hdf5:
+            output_hdf5 = self.input_dir+"features.hdf5"
+        self.output_data = h5py.File(output_hdf5, "w")
 
     def get_annotations(self, ct_scan, patient):
         ct_scan = np.array(ct_scan)
@@ -49,24 +55,43 @@ class NodulesPrepper():
         # Fill bounding volume
         bound_volume = ct_scan[x_:_x,y_:_y,z_:_z]
         # Write to output hdf5 file
-        self.output_hdf5.create_dataset(patient, data=bound_volume)
+        self.output_data.create_dataset(patient, data=bound_volume)
         return
 
     def process(self):
         # Load dataset
         print("Loading data")
-        data = h5py.File(self.input_hdf5, "r")
+        input_data = h5py.File(self.input_hdf5, "r")
         pattern = re.compile(self.patient_regex)
-        patients = [k for k in list(data.keys()) if pattern.match(k)]
+        patients = [k for k in list(input_data.keys()) if pattern.match(k)]
         print("Parsing patients")
         # Run annotation search
         for patient in patients:
             print("Processing {}".format(patient))
-            self.get_annotations(data.get(patient), patient)
+            self.get_annotations(input_data.get(patient), patient)
         print("Writing to disk")
-        self.output_hdf5.close()
+        self.output_data.close()
         return
 
 if __name__ == "__main__":
-    prepper = NodulesPrepper()
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-i", "--input_hdf5", 
+        help="path to input hdf5 file", 
+        type=str, 
+        default="")
+    parser.add_argument(
+        "--output_hdf5", 
+        help="(optional) path to output hdf5 file", 
+        type=str, 
+        default="")
+    args = parser.parse_args()
+
+    prepper = NodulesPrepper(
+        input_hdf5 = args.input_hdf5,
+        output_hdf5 = args.output_hdf5,
+        bounding_volume_shape=(64,64,32),
+        patient_regex="^[A-Z][a-z]+_ser_\d+$"
+    )
+
     prepper.process()
