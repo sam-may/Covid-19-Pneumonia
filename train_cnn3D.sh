c6bfd1f095b9b0c1dccf76e7d6cd24e03c480108
Jonathan Guiang <jkguiang@gmail.com>
2020-09-11 20:32:16 +0000

    fixed

--- train_cnn3D.sh
+++ train_cnn3D.sh
@@ -1,8 +1,7 @@
 python train_cnn3D.py \
 --data_hdf5="/mnt/data/LungNodules/features_rotatable.hdf5" \
 --metadata_json="/mnt/data/LungNodules/features_rotatable_metadata.json" \
-# Tag format: v-A-B-C where A --> extra features, B --> rotations, C --> Nothing
---tag="nodulesCNN3D_v1-0-0" \
+--tag="nodulesCNN3D_v1-1-0" \
 --max_epochs=100 \
 --n_trainings=5 \
 --training_batch_size=16 \
@@ -12,7 +11,12 @@ python train_cnn3D.py \
 --random_seed=0 \
 --train_frac=0.7 \
 --loss_function="weighted_crossentropy" \
-# None: 0, volume: 1, COM: 2, volume+COM: 3
 --extra_features nodule_volume_norm \
-# None: 0, azim: 1, polar: 2, azim+polar: 3
-# --do_rotations
+--do_rotations
+
+# Tag format: v-A-B-C
+#     A --> extra features
+#     None: 0, volume: 1, COM: 2, volume+COM: 3
+#     B --> rotations
+#     None: 0, azim: 1, polar: 2, azim+polar: 3
+#     C --> nothing
